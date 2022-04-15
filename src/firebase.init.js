148146891cd2786b669c295fd0a3085230c58259
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-04-15 18:51:37 +0600

    ready

---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcPmTT7f_iCvITR_wEcvJOTPL5X_afYR4",
+  authDomain: "red-onion-5ebb7.firebaseapp.com",
+  projectId: "red-onion-5ebb7",
+  storageBucket: "red-onion-5ebb7.appspot.com",
+  messagingSenderId: "1063606238918",
+  appId: "1:1063606238918:web:36c91cb2104e406cca21c7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
